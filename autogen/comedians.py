0382fc5cfb8f49b5070bc48b1f4f88ccbc8c22e9
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2025-03-25 16:41:36 +0100

    feat: Update comedians.py to enable Ollama model and enhance system message for Joe

--- autogen/comedians.py
+++ autogen/comedians.py
@@ -11,10 +11,10 @@ async def main() -> None:
       model="gpt-4o",
   )
 
-  # ollama_model_client = OpenAIChatCompletionClient(
-  #     model="phi3",
-  #     base_url="http://127.0.0.1:11434/v1",
-  # )
+  ollama_model_client = OpenAIChatCompletionClient(
+      model="phi3",
+      base_url="http://127.0.0.1:11434/v1",
+  )
 
   # Start logging with logger_type and the filename to log to
   # logging_session_id = autogen.runtime_logging.start(logger_type="sqlite", config={"dbname": "logs.db"})
@@ -31,12 +31,12 @@ async def main() -> None:
 
   joe = AssistantAgent(
       name="joe",
-      system_message="Your name is Joe and you are a part of a duo of comedians.",
-      model_client=openai_model_client
+      system_message="Your name is Joe and you are a part of a duo of comedians and you like dad jokes.",
+      model_client=ollama_model_client
   )
 
   group_chat = RoundRobinGroupChat([cathy, joe], max_turns=4)
-  stream = group_chat.run_stream(task="Cathy, tell me a joke.")
+  stream = group_chat.run_stream(task="Cathy, tell me a joke about progamming")
   await Console(stream)
 
   # agentops.end_session("Success")
