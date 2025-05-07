c2f0b0ef9d8bb2dbcb00178481ee527727c1f0af
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2025-05-07 13:41:40 +0200

    fix: Chrome own browser use

--- browser-use/linkedin.py
+++ browser-use/linkedin.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-from browser_use import Agent, Browser, BrowserConfig
+from browser_use import Agent, Browser, BrowserConfig, Controller
 import asyncio
 from dotenv import load_dotenv
 
@@ -8,8 +8,15 @@ load_dotenv()
 controller = Controller()
 browser = Browser(
     config=BrowserConfig(
-        # Use my own chrome browser
         browser_binary_path="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+
+        # Chrome's Security Features don't allow remote debugging to be enabled when the browser is
+        # using the default user data directory. In this workaroung, a remote-debug-profile gets created.
+        # This profile will not have any of your existing cookies or logins, but will store them across
+        # sessions. You can delete the remote-debug-profile directory to clear the cookies.
+        extra_browser_args=[
+            "--user-data-dir=remote-debug-profile",
+        ],
     )
 )
 
@@ -22,9 +29,9 @@ async def main():
     agent = Agent(
         task=(
             "Open LinkedIn. "
-            "Search for Gustav Galata. Send him a message. Ask the user for the message content. "
+            "Search for Bill Gates. Send him a message. Ask the user for the message content. "
         ),
-        # browser=browser,
+        browser=browser,
         llm=ChatOpenAI(model="gpt-4.1"),
         controller=controller
     )
